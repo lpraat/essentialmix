5c1daa7c107b0bf882d8f021b1d166a6d2fc463b
Lorenzo Pratissoli <lorenzo.pratissoli@outlook.com>
2023-06-02 18:03:11 +0200

    Add attention layer

---
+++ essentialmix/core/layers.py
@@ -0,0 +1,75 @@
+from typing import Optional
+
+import torch
+import torch.nn as nn
+from einops import einsum, rearrange
+
+
+def left_to_right_attention_mask(seq_len: int) -> torch.Tensor:
+    return torch.tril(torch.ones(seq_len, seq_len))
+
+
+class MultiHeadAttention(nn.Module):
+    """
+    Attention with d_q = d_k = d_v
+    """
+
+    def __init__(
+        self,
+        n_heads: int,
+        emb_dim: int,
+        bias: bool = False,
+    ) -> None:
+        super().__init__()
+        assert emb_dim % n_heads == 0, "Embedding dimension must be divisible by the number of heads"
+        self.n_heads = n_heads
+        self.d = emb_dim // self.n_heads
+        self.emb_dim = emb_dim
+        self.register_buffer("scale", torch.sqrt(torch.tensor([self.d])))
+        self.proj_q = nn.Linear(emb_dim, self.n_heads * self.d, bias=bias)
+        self.proj_k = nn.Linear(emb_dim, self.n_heads * self.d, bias=bias)
+        self.proj_v = nn.Linear(emb_dim, self.n_heads * self.d, bias=bias)
+        self.proj_o = nn.Linear(emb_dim, emb_dim, bias=bias)
+
+    def forward(
+        self,
+        q: torch.Tensor,
+        k: torch.Tensor,
+        v: torch.Tensor,
+        mask: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        assert q.shape == k.shape == v.shape
+        rearrange_pattern = "batch time (heads dim) -> batch heads time dim"
+        query = rearrange(self.proj_q(q), rearrange_pattern, heads=self.n_heads)
+        key = rearrange(self.proj_k(k), rearrange_pattern, heads=self.n_heads)
+        value = rearrange(self.proj_v(v), rearrange_pattern, heads=self.n_heads)
+
+        attn = (
+            einsum(
+                query,
+                key,
+                "batch heads time1 dim, batch heads time2 dim -> batch heads time1 time2",
+            )
+            / self.scale
+        )
+
+        if mask is not None:
+            assert mask.shape[-1] == mask.shape[-2] == q.shape[1]
+            # (batch, heads, time, time)
+            mask = mask.expand(query.shape[0], self.n_heads, mask.shape[-1], mask.shape[-1])
+            attn.masked_fill_(mask == 0, float("-inf"))
+
+        attn = attn.softmax(dim=-1)
+        attn = einsum(
+            attn,
+            value,
+            "batch heads time1 time2, batch heads time2 dim -> batch heads time1 dim",
+        )
+        return self.proj_o(
+            rearrange(
+                attn,
+                "batch heads time dim -> batch time (heads dim)",
+                heads=self.n_heads,
+                dim=self.d,
+            )
+        )
---
+++ tests/test_layers.py
@@ -0,0 +1,40 @@
+import pytest
+import torch
+
+from essentialmix.core.layers import MultiHeadAttention, left_to_right_attention_mask
+
+
+@pytest.mark.parametrize(
+    "batch_size,seq_len,emb_dim,n_heads,mask",
+    [
+        (1, 3, 16, 2, None),
+        (1, 3, 16, 2, left_to_right_attention_mask(3)),
+        (3, 2, 8, 1, None),
+        (3, 2, 8, 1, left_to_right_attention_mask(2)),
+        (3, 2, 9, 3, None),
+        (3, 2, 9, 3, left_to_right_attention_mask(2)),
+    ],
+)
+def test_multi_head_attention(batch_size: int, seq_len: int, emb_dim: int, n_heads: int, mask: torch.Tensor) -> None:
+    input_ = torch.randn((batch_size, seq_len, emb_dim))
+    mha = MultiHeadAttention(n_heads=n_heads, emb_dim=emb_dim)
+    torch.testing.assert_close(mha.scale**2, torch.tensor([emb_dim / n_heads]))
+    attn = mha(q=input_, k=input_, v=input_, mask=mask)
+    assert attn.shape == (batch_size, seq_len, emb_dim)
+
+
+@pytest.mark.parametrize(
+    "batch_size,seq_len,emb_dim,n_heads,mask",
+    [
+        (3, 2, 9, 2, None),
+        (3, 2, 9, 2, left_to_right_attention_mask(2)),
+        (3, 2, 9, 3, left_to_right_attention_mask(3)),
+    ],
+)
+def test_multi_head_attention_with_err(
+    batch_size: int, seq_len: int, emb_dim: int, n_heads: int, mask: torch.Tensor
+) -> None:
+    input_ = torch.randn((batch_size, seq_len, emb_dim))
+    with pytest.raises(AssertionError):
+        mha = MultiHeadAttention(n_heads=n_heads, emb_dim=emb_dim)
+        mha(q=input_, k=input_, v=input_, mask=mask)
